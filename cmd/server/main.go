53525598301322b828955dff26f1753e02f5046e
kvisidisi <lyapunovvadim5002@gmail.com>
2025-07-23 12:19:27 +0300

    feat: logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/kv1sidisi/skrepka/internal/config"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"io"
 	"log/slog"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 // health handles the health check endpoint.
@@ -38,6 +41,11 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
+	cfg := config.MustLoad()
+
+	logger = SetupLogger(cfg.Env, cfg.LogPath)
+	slog.SetDefault(logger)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", health)
 	mux.Handle("/metrics", promhttp.Handler())
@@ -48,4 +56,34 @@ func main() {
 		slog.Error("server failed to start", "error", err)
 		os.Exit(1)
 	}
+}
+
+// SetupLogger used to set up logger based on env level.
+// returns slog.Logger on success
+func SetupLogger(env, logPath string) *slog.Logger {
+	var logger *slog.Logger
+
+	logDir := filepath.Dir(logPath)
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		slog.Error("failed to create log directory", "error", err)
+	}
+
+	logFile, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		slog.Error("failed to open log file", "error", err)
+	}
+
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+
+	switch env {
+	case "debug":
+		logger = slog.New(slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "local":
+		logger = slog.New(slog.NewTextHandler(multiWriter, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "prod":
+		logger = slog.New(slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		logger = slog.New(slog.NewJSONHandler(multiWriter, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return logger
 }
